870c10ed790ac16a525a6dbe4b2fca70882788dd
mty <mty_22968712@163.com>
2018-12-27 21:00:33 +0800

    短信验证码-基本业务逻辑

---
+++ meiduo_test/meiduo_test/apps/verifications/constants.py
@@ -0,0 +1,5 @@
+# redis 中短信验证码的有效时间 ：s
+SMS_CODE_REDIS_EXPIRES = 300
+
+# 发送短信模板id
+SEND_SMS_TEMP_ID = 1
---
+++ meiduo_test/meiduo_test/apps/verifications/views.py
@@ -0,0 +1,57 @@
+import random
+
+from django.shortcuts import render
+from django_redis import get_redis_connection
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+from verifications import constants
+
+# Create your views here.
+
+# 获取redis链接
+# from redis import StrictRedis
+# redis_conn = StrictRedis(host=<ip>, port=<port>, db=<db>)
+
+# 获取日志器
+import logging
+logger = logging.getLogger('django')
+
+
+# GET /sms_codes/(?P<mobile>1[3-9]\d{9})/
+class SMSCodeView(APIView):
+    def get(self, request, mobile):
+        """
+        短信验证码的获取:
+        1. 随机生成6位数字作为短信验证码
+        2. 在redis中保存短信验证码的内容，以`mobile`为key，以`验证码内容`为value
+        3. 使用云通讯发送短信验证码
+        4. 返回应答，发送成功
+        """
+        # 1. 随机生成6位数字作为短信验证码
+        sms_code = '%06d' % random.randint(0, 999999) # 000010
+
+        # 2. 在redis中保存短信验证码的内容，以`mobile`为key，以`验证码内容`为value
+        redis_conn = get_redis_connection('verify_codes')  # StrictRedis对象
+
+        # redis_conn.set('<key>', '<value>', '<expries>')
+        # redis_conn.setex('<key>', '<expries>', '<value>')
+
+        redis_conn.setex('sms_%s' % mobile, constants.SMS_CODE_REDIS_EXPIRES, sms_code)
+
+        # 3. 使用云通讯发送短信验证码
+        expires = constants.SMS_CODE_REDIS_EXPIRES // 60
+        # try:
+        #     res = CCP().send_template_sms(mobile, [sms_code, expires], constants.SEND_SMS_TEMP_ID)
+        # except Exception as e:
+        #     logger.error('发送短信异常: %s' % e)
+        #     return Response({'message': '短信发送异常'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+        #
+        # if res != 0:
+        #     # 发送短信失败
+        #     return Response({'message': '短信发送失败'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+
+        # 4. 返回应答，发送成功
+        return Response({'message': 'OK'})
