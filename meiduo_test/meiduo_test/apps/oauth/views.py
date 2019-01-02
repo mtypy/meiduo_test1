da922fe9dc2ae5ea77f64d9c4623ef22dd5f43dd
mty <mty_22968712@163.com>
2019-01-02 19:07:50 +0800

    qq登陆step3-保存

--- meiduo_test/meiduo_test/apps/oauth/views.py
+++ meiduo_test/meiduo_test/apps/oauth/views.py
@@ -1,51 +1,73 @@
 from django.shortcuts import render
-
-# Create your views here.
-# GET /oauth/qq/authorization/?next=<登陆之后跳转页面的地址>
 from rest_framework import status
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from oauth.exceptions import QQAPIError
 from oauth.models import OAuthQQUser
+from oauth.serializers import QQAuthUserSerializer
 from oauth.utils import OAuthQQ
 
+# Create your views here.
 
-#  GET /oauth/qq/user/?code=<code>
-class QQAuthUserView(APIView):
+
+# GET /oauth/qq/user/?code=<code>
+class QQAuthUserView(GenericAPIView):
+    serializer_class = QQAuthUserSerializer
+
+    def post(self, request):
+        """
+        保存QQ登录用户绑定数据:
+        1. 获取参数并进行校验(参数完整性，手机号格式，短信验证是否正确，access_token是否有效)
+        2. 保存绑定用户的数据并签发jwt token
+        3. 返回应答，绑定成功
+        """
+        # 1. 获取参数并进行校验(参数完整性，手机号格式，短信验证是否正确，access_token是否有效)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        # 2. 保存绑定用户的数据并签发jwt token(create)
+        serializer.save()
+
+        # 3. 返回应答，绑定成功
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
     def get(self, request):
         """
-        获取qq登陆用户的openid并处理
-        1.获取code并校验 （code必传）
-        2.获取qq登陆用户的openid
-            2.1 根据code 请求qq服务器获取access_token
-            2.2 根据access_token请求qq服务器获取openid
-        3.根据openid判断是否绑定过本网站的用户
+        获取QQ登录用户的openid并处理:
+        1. 获取code并校验(code必传)
+        2. 获取QQ登录用户的openid
+            2.1 根据code请求QQ服务器获取access_token
+            2.2 根据access_token请求QQ服务器获取openid
+        3. 根据openid判断是否绑定过本网站的用户
             3.1 如果已绑定，直接生成jwt token并返回
             3.2 如果未绑定，将openid加密并返回
-
         """
+        # 1. 获取code并校验(code必传)
+        code = request.query_params.get('code') # None
 
-        # 1.获取code并校验 （code必传）
-        code = request.query_params.get("code")  # None
         if code is None:
-            return Response({"message": "缺少code参数"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': '缺少code参数'}, status=status.HTTP_400_BAD_REQUEST)
 
-        # 2.获取qq登陆用户的openid
+        # 2. 获取QQ登录用户的openid
         oauth = OAuthQQ()
 
-        # 2.1根据code请求qq服务器获取access_token
-        access_token = oauth.get_access_token(code)
-        # 2.2根据access_token请求qq服务器获取openid
-        openid = oauth.get_openid(access_token)
+        try:
+            # 2.1 根据code请求QQ服务器获取access_token
+            access_token = oauth.get_access_token(code)
+            # 2.2 根据access_token请求QQ服务器获取openid
+            openid = oauth.get_openid(access_token)
+        except QQAPIError:
+            return Response({'message': 'QQ服务异常'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
 
-        # 3.根据openid判断是否绑定过本网站的用户
+        # 3. 根据openid判断是否绑定过本网站的用户
         try:
             qq_user = OAuthQQUser.objects.get(openid=openid)
         except OAuthQQUser.DoesNotExist:
-            # 3.2如果未绑定，将openid加密并返回
+            # 3.2 如果未绑定，将openid加密并返回
             secret_openid = oauth.generate_save_user_token(openid)
-            return Response({"access_token": secret_openid})
-
+            return Response({'access_token': secret_openid})
         else:
             # 3.1 如果已绑定，直接生成jwt token并返回
             user = qq_user.user
@@ -62,27 +84,32 @@ class QQAuthUserView(APIView):
             # 生成jwt token
             token = jwt_encode_handler(payload)
 
+            # 组织响应数据并返回
             response_data = {
-                "user_id": user.id,
-                "user_name": user.name,
-                "token": token
+                'user_id': user.id,
+                'username': user.username,
+                'token': token
             }
 
+            return Response(response_data)
 
+
+# GET /oauth/qq/authorization/?next=<登录之后跳转页面的地址>
 class QQAuthURLView(APIView):
     def get(self, request):
         """
-         获取QQ登陆网址：
-         1. 获取next (可以不传)
-         2. 组织qq登陆网址和参数
-         3. 返回QQ登陆网址
-         """
-        # 1. 获取next (可以不传)
-        request.query_params.get("next", "/")  # 如果没有传 默认跳转到“/”
+        获取QQ登录网址:
+        1. 获取next(可以不传)
+        2. 组织QQ登录网址和参数
+        3. 返回QQ登录网址
+        """
+        # 1. 获取next(可以不传)
+        next = request.query_params.get('next', '/')
 
-        # 2. 组织qq登陆网址和参数
+        # 2. 组织QQ登录网址和参数
         oauth = OAuthQQ(state=next)
+
         login_url = oauth.get_login_url()
 
-        # 3. 返回QQ登陆网址
-        return Response({"login_url": login_url})
+        # 3. 返回QQ登录网址
+        return Response({'login_url': login_url})
